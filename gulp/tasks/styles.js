39c915b1f45ccd72e49bdf12afa484868c0bfe9b
maverick <maverick@localhost.localdomain>
2018-03-27 20:58:33 +0200

    added error handling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ cssImport = require('postcss-import');
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/style.css')
     .pipe(postcss([cssImport, nested, cssVars, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 })
