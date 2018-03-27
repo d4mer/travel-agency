0ceef78fc1749424e8f60ca7848cbc1b6a5961e7
maverick <maverick@localhost.localdomain>
2018-03-27 20:40:53 +0200

    removed css vars and task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,9 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssVars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
 browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
   console.log('Wow, it works');
-})
-
-gulp.task('styles', function(){
-  return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport, nested, cssVars, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
 })
 
 gulp.task('watch', function(){
