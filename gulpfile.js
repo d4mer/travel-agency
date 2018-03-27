27747f992d35f2d189df311eddf3cd3da456640e
maverick <maverick@localhost.localdomain>
2018-03-27 19:30:23 +0200

    added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync');
 
 gulp.task('default', function(){
   console.log('Wow, it works');
